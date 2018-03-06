bca92037fc3de93ec4dad9c58b784d7105b4aeb3
azhry <arliansyah_azhary@yahoo.com>
2018-03-06 22:49:06 +0700

    Change react native version to 0.54.0-rc.3 for installing buggy react-native-maps and hide component life cycle deprecation warning message

--- components/BaseTabs.js
+++ components/BaseTabs.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList, TouchableOpacity } from 'react-native';
-import { Body, Container, Content, Header, Tab, Tabs, Text } from 'native-base';
+import { FlatList, TouchableOpacity, View, NetInfo } from 'react-native';
+import { Body, Container, Content, Header, Spinner, Tab, Tabs, Text, Toast } from 'native-base';
 import KajianCard from './KajianCard';
+
+const BASE_URL  = 'http://192.168.43.144/kajian/';
 
 export default class BaseTabs extends Component {
 
@@ -9,53 +11,116 @@ export default class BaseTabs extends Component {
 
     super( props );
 
-    // using dummy data for testing purpose, will add fetch api later
     this.state = { 
-      data: [
-        {
-          title: 'Sholat Sunnah yang Menyamai Pahala Sholat Wajib',
-          lecturer: 'Ustadz Mizan Qudsiyah, Lc',
-          imgSrc: 'http://placehold.it/350x200'
-        }
-      ]
+      data: null,
+      connected: false
     };
+
+  }
+
+  componentDidMount() {
+
+    this._fetchAPI();
+    NetInfo.isConnected.addEventListener( 'change', this._handleConnectionChange );
+
+  }
+
+  _handleConnectionChange() {
+
+    NetInfo.isConnected.fetch()
+      .then(isConnected => {
+        this.setState({ connected: isConnected });
+        if ( isConnected ) {
+          _fetchAPI();
+        }
+      });
+
+  }
+
+  _fetchAPI() {
+
+    fetch(BASE_URL + 'service', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'param1=param_1&param2=param_2'
+      })
+      .then(( response ) => response.json())
+      .then(( responseJson ) => {
+        
+        this.setState({ data: responseJson });
+        console.log( responseJson );
+      
+      })
+      .catch(( error ) => {
+        Toast.show({
+          text: 'No internet connection!',
+          position: 'bottom',
+          buttonText: 'Close'
+        });
+      });
 
   }
 
   render() {
 
-    const { navigate } = this.props.navigation;
+    if ( this.state.data == null ) {
 
-    return (
-      <Container>
-        <Tabs initialPage={0}>
-          <Tab heading="Kajian">
-            
-            <Content>
-              <FlatList
-                data={ this.state.data }
-                renderItem={ ({ item }) =>
-                  <TouchableOpacity onPress={ () => navigate( 'KajianDetail', { title: item.title } ) }>
-                    <KajianCard 
-                      title={ item.title } 
-                      lecturer={ item.lecturer }
-                      imgSrc={ item.imgSrc } />
-                  </TouchableOpacity>
+      return (
+        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+          <Spinner color='blue' />
+        </View>
+      );
+    
+    } 
+    // else if ( this.state.connected == false ) {
 
-                }
-                keyExtractor={ ( item, index ) => index.toString() }
-              />
-            </Content>
+    //   return (
+    //     <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+    //       <Text>No internet connection!</Text>
+    //     </View>
+    //   );
 
-          </Tab>
-          <Tab heading="Playlist">
-            
-            {/* Playlist implementation here */}
+    // } 
+    else {
 
-          </Tab>
-        </Tabs>
-      </Container>
-    );
+      const { navigate } = this.props.navigation;
+
+      return (
+        <Container>
+          <Tabs initialPage={0}>
+            <Tab heading="Kajian">
+              
+              <Content>
+                <FlatList
+                  data={ this.state.data }
+                  renderItem={ ({ item }) =>
+                    <TouchableOpacity onPress={ () => navigate( 'KajianDetail', { title: item.title } ) }>
+                      <KajianCard 
+                        title={ item.title } 
+                        lecturer={ item.lecturer }
+                        imgSrc={ item.imgSrc } />
+                    </TouchableOpacity>
+
+                  }
+                  keyExtractor={ ( item, index ) => index.toString() }
+                />
+              </Content>
+
+            </Tab>
+            <Tab heading="Playlist">
+              
+              {/* Playlist implementation here */}
+
+            </Tab>
+          </Tabs>
+        </Container>
+      );
+
+    }
+    
 
   }
 }
--- components/KajianDetail.js
+++ components/KajianDetail.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Image, TouchableOpacity, StyleSheet, View } from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
-import { Body, Card, CardItem, Content, Container, Header, Left, Text, Title } from 'native-base';
+import { Body, Button, Card, CardItem, Content, Container, Header, Left, Text, Title } from 'native-base';
 import Icon from 'react-native-vector-icons/Ionicons';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class KajianDetail extends Component {
 
 	static navigationOptions = {
 		header: false
 	};
+
+	constructor( props ) {
+
+		super( props );
+		this.state = {
+			region: {
+				latitude: 37.78825,
+				longitude: -122.4324,
+				latitudeDelta: 0.0922,
+				longitudeDelta: 0.0421,
+			}
+		};
+
+	}
 
 	render() {
 
@@ -47,6 +62,44 @@ export default class KajianDetail extends Component {
             							</Text>
             						</Col>
 								</Grid>
+								<Grid>
+									<Col style={{ height: 250 }}>
+										<View style={ styles.container }>
+											<MapView
+												provider={PROVIDER_GOOGLE}
+												style={ styles.map }
+												region={this.state.region} />
+										</View>
+									</Col>
+								</Grid>
+								<Grid>
+									<Col style={{ padding: 10 }}>
+										<Text style={{ fontSize: 18 }} >
+											Apakah akan hadir?
+										</Text>
+									</Col>
+            						<Col style={{ padding: 10 }}>
+        								<Content>
+								          <Button light><Text> Light </Text></Button>
+								          <Button primary><Text> Primary </Text></Button>
+								          <Button success><Text> Success </Text></Button>
+								          <Button info><Text> Info </Text></Button>
+								          <Button warning><Text> Warning </Text></Button>
+								          <Button danger><Text> Danger </Text></Button>
+								          <Button dark><Text> Dark </Text></Button>
+								        </Content>
+            						</Col>
+								</Grid>
+							</Body>
+						</CardItem>
+					</Card>
+					<Card>
+						<CardItem>
+							<Body style={ styles.cardBody }>
+								<Text style={{ fontSize: 23 }}>Detail</Text>
+								<Text>
+									Akan dilaksanakan kajian bertema Sholat Sunnah yang Menyamai Pahala Sholat Wajib. Insya Allah akan dilaksanakan di Masjid Bakti. Jln. Subakti Palembang pada hari Rabu waktu setelah Ba'da Maghrib
+								</Text>
 							</Body>
 						</CardItem>
 					</Card>
@@ -72,5 +125,15 @@ const styles = StyleSheet.create({
 	},
 	cardBody: {
 		opacity: 0.6
-	}
+	},
+	container: {
+	    ...StyleSheet.absoluteFillObject,
+	    height: 250,
+	    width: 400,
+	    justifyContent: 'flex-end',
+	    alignItems: 'center',
+	},
+ 	map: {
+    	...StyleSheet.absoluteFillObject,
+  	}
 });
