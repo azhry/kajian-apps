30d3920cf3e6ae1df8c4797a2bd5ee305f3d811b
azhry <arliansyah_azhary@yahoo.com>
2018-04-15 20:47:08 +0700

    linking react-native-firebase module

--- android/app/src/main/java/com/kajianreactapp/MainApplication.java
+++ android/app/src/main/java/com/kajianreactapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.kajianreactapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new ReactNativeYouTube(),
             new VectorIconsPackage(),
             new MapsPackage(),
